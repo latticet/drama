2318e5494565003355e7c517148e0c3087582155
“latticet” <latticet@163.com>
2016-09-25 17:36:02 +0800

    添加token验证

--- app.js
+++ app.js
@@ -5,14 +5,29 @@ var sha1 = require('sha1');
 
 var config = {
     wechat:{
-
+        appId: 'wx6db38a33f2879d49',
+        appSecret: 'ba44eb2f7c4d62af877607f2380a6f54',
+        token: 'yaowenzhang3321'
     }
 }
 
 var app = new Koa();
 
 app.use(function *(){
-    this.body = 'Hello World';
+
+    console.log(this.query);
+
+    var token = config.wechat.token;
+    var signature = this.query.signature;
+    var timestamp = this.query.timestamp;
+    var nonce = this.query.nonce;
+    var echostr = this.query.echo.str;
+
+    var str = [token, timestamp, nonce].sort().join("");
+    var sha = sha1(str);
+
+    sha === signature ? this.body = echostr : this.body = 'wrong';
+
 })
 
 app.listen(3000);
