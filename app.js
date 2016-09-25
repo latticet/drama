161ca051a8464809314c9a7e4b34d8fa1594aa53
“latticet” <latticet@163.com>
2016-09-25 17:15:18 +0800

    添加koa

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+'use strict';
+
+var Koa = require('Koa');
+var sha1 = require('sha1');
+
+var config = {
+    wechat:{
+
+    }
+}
+
+var app = new Koa();
+
+app.use(function *(){
+    this.body = 'Hello World';
+})
+
+app.listen(3000);
+console.log('Listening : 3000');
